a850d7da568293a5bfe1b6327e478d32272f53f5
de4ltt <aleksejtmk@gmail.com>
2024-05-01 12:08:32 +0300

    cringe commit

--- Android/app/src/main/java/com/kire/market_place_android/presentation/screen/admin_panel_items_edit_screen_ui/AdminPanelIconField.kt
+++ Android/app/src/main/java/com/kire/market_place_android/presentation/screen/admin_panel_items_edit_screen_ui/AdminPanelIconField.kt
@@ -2,28 +2,8 @@ package com.kire.market_place_android.presentation.screen.admin_panel_items_edit
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import com.kire.market_place_android.presentation.theme.ExtendedTheme
 
 @Composable
 fun AdminPanelIconField(
@@ -32,7 +12,7 @@ fun AdminPanelIconField(
     @StringRes hintText: Int,
     maxLines: Int = 1
 ) {
-    var textValue by remember {
+    /*var textValue by remember {
         mutableStateOf("")
     }
 
@@ -74,5 +54,5 @@ fun AdminPanelIconField(
         }
     }
 
-
+*/
 }
