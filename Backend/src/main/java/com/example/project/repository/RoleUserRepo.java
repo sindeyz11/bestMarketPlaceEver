c310abff999cbcf9f5246c8243b4afb888a52693
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-05-18 20:41:46 +0300

    remove repo

--- Backend/src/main/java/com/example/project/repository/RoleUserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.entity.RoleUser;
-import com.example.project.entity.pk.ID_Role_user;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleUserRepo extends JpaRepository<RoleUser, ID_Role_user> {
-}
