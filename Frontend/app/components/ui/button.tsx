2c85a55c32cc7dc515b9864a4dd34faa04eedfdf
khudobin-v <162823979+khudobin-v@users.noreply.github.com>
2024-05-22 02:23:07 +0300

    Создание компонента кнопки

--- Frontend/app/components/ui/button.tsx
+++ Frontend/app/components/ui/button.tsx
@@ -1,3 +1,57 @@
-export const Button = () => {
-  return <>Button</>;
+"use client";
+
+import Link from "next/link";
+
+interface IButton {
+  variant?: "default" | "icon" | "link";
+  icon?: React.ReactNode;
+  color?: "light" | "dark";
+  href?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
+  children?: React.ReactNode;
+}
+
+export const Button = ({
+  variant = "default",
+  icon,
+  color = "light",
+  href,
+  onClick,
+  children,
+}: IButton) => {
+  return (
+    <>
+      {/* текстовая кнопка */}
+      {variant === "default" && (
+        <button
+          onClick={onClick}
+          className={`w-full rounded-lg p-3 text-white font-semibold flex items-center justify-center ${
+            color === "light"
+              ? "bg-button-accent hover:bg-button-accent/85"
+              : "bg-button-dark-accent hover:bg-button-dark-accent/85"
+          } transition-colors`}
+        >
+          {children}
+        </button>
+      )}
+      {/* кнопка-иконка */}
+      {variant === "icon" && icon && (
+        <button
+          onClick={onClick}
+          className="w-max rounded-lg p-3 text-icon bg-transparent hover:text-icon/90 hover:bg-transparent/10 transition-colors flex items-center justify-center"
+        >
+          {icon}
+        </button>
+      )}
+      {/* кнопка-ссылка */}
+      {variant === "link" && href && (
+        <Link
+          className="text-link underline underline-offset-2 font-medium hover:text-link/80 hover:underline-offset-4 transition-all"
+          href={href}
+        >
+          {children}
+        </Link>
+      )}
+    </>
+  );
 };
