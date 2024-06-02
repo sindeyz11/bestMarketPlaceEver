3e7e0157c7353f58541d382504009df0391861de
anna-lipchanskaya <144255662+anna-lipchanskaya@users.noreply.github.com>
2024-06-02 20:25:27 +0300

    Validation

--- Backend/src/main/java/com/example/project/common/Constants.java
+++ Backend/src/main/java/com/example/project/common/Constants.java
@@ -3,11 +3,13 @@ package com.example.project.common;
 public final class Constants {
     public static final String NOT_FOUND_PRODUCT = "Product not found, incorrect id: ";
     public static final String NOT_FOUND_PICKUPPOINT = "Pickup point not found";
-    public static final String NOT_FOUND_USER = "User not found";
+    public static final String NOT_FOUND_USER = "Войдите или зарегистрируйтесь";
     public static final String NOT_FOUND_MANAGER = "Manager not found";
-    public static final String INCORRECT_PASSWORD = "Incorrect password";
-    public static final String MISMATCH_PASSWORD = "Mismatch password";
-    public static final String UNIQUE_EMAIL = "Email is not unique";
-    public static final String UNIQUE_PHONE = "Phone is not unique";
-    public static final String INVALID_LOGIN_OR_PASSWORD = "Login or password is incorrect";
+    public static final String INCORRECT_PASSWORD = "Неправильный пароль";
+    public static final String MISMATCH_PASSWORD = "Пароли не совпадают";
+    public static final String UNIQUE_EMAIL = "Такая почта уже используется";
+    public static final String UNIQUE_PHONE = "Такой телефон уже используется";
+    public static final String INVALID_LOGIN_OR_PASSWORD = "Неправильный логин или пароль";
+    public static final String INCORRECT_DATE = "Неправильная дата";
 }
+
--- Backend/src/main/java/com/example/project/dto/request/ChangePasswordRequest.java
+++ Backend/src/main/java/com/example/project/dto/request/ChangePasswordRequest.java
@@ -1,5 +1,7 @@
 package com.example.project.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +10,10 @@ import lombok.Setter;
 @Setter
 @Builder
 public class ChangePasswordRequest {
+    @NotBlank(message = "Пароль неверный")
     private String currentPassword;
+    @NotBlank(message = "Неправильный пароль")
+    @Pattern(regexp = "^$|^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?`~]{8,100}$", message = "Неправильный пароль")
     private String newPassword;
     private String confirmationPassword;
 }
